657382f0f0236c35a9b68a15dc48944da4522391
Robert <jeszczemoge@gmail.com>
2014-10-12 13:13:10 +0200

    Read layers

--- src/pl/edu/pw/mini/bms2/Main.java
+++ src/pl/edu/pw/mini/bms2/Main.java
@@ -3,9 +3,32 @@ package pl.edu.pw.mini.bms2;
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.nnet.MultiLayerPerceptron;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        NeuralNetwork nn = new MultiLayerPerceptron(5, 10, 1);
+        int hiddenLayers, inputNeurons;
+        List<Integer> neuronsInLayers = new ArrayList<Integer>();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Number of input neurons");
+        inputNeurons = in.nextInt();
+        neuronsInLayers.add(inputNeurons);
+        System.out.println("Number of hidden layers");
+        hiddenLayers = in.nextInt();
+
+        for (int i = 1; i <= hiddenLayers; i++) {
+            System.out.println("Number of neurons in hidden layer " + i);
+            int tmp = in.nextInt();
+            neuronsInLayers.add(tmp);
+        }
+
+        //add output layer
+        neuronsInLayers.add(1);
+
+        NeuralNetwork nn = new MultiLayerPerceptron(neuronsInLayers);
     }
 }
