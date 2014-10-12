2fc015e9c8a68fdf29d160b16a981305168fe4a3
Robert <jeszczemoge@gmail.com>
2014-10-12 15:18:23 +0200

    Bias setting

--- src/pl/edu/pw/mini/bms2/Main.java
+++ src/pl/edu/pw/mini/bms2/Main.java
@@ -1,7 +1,12 @@
 package pl.edu.pw.mini.bms2;
 
+import org.neuroph.core.Layer;
 import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.Neuron;
 import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.nnet.comp.neuron.BiasNeuron;
+import org.neuroph.util.NeuronProperties;
+import org.neuroph.util.TransferFunctionType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +34,24 @@ public class Main {
         //add output layer
         neuronsInLayers.add(1);
 
-        NeuralNetwork nn = new MultiLayerPerceptron(neuronsInLayers);
+        System.out.println("Use bias? (Y/N)");
+        String biasString;
+        boolean bias;
+
+        while(true) {
+            biasString = in.next();
+            if (biasString.equals("Y") || biasString.equals("y")) {
+                bias = true;
+                break;
+            } else if (biasString.equals("N") || biasString.equals("n"))  {
+                bias = false;
+                break;
+            } else {
+                System.out.println("Try again. (Y/N)");
+            }
+        }
+
+        NeuronProperties np = new NeuronProperties(TransferFunctionType.LINEAR, bias);
+        NeuralNetwork nn = new MultiLayerPerceptron(neuronsInLayers, np);
     }
 }
