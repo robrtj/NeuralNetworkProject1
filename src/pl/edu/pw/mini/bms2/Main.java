2a434edd0843371b37cbb16dcdc7363f3a0c688b
Paweł Bielicki <bielicki91@gmail.com>
2014-10-12 16:36:43 +0200

    csv parser

--- src/pl/edu/pw/mini/bms2/Main.java
+++ src/pl/edu/pw/mini/bms2/Main.java
@@ -1,14 +1,21 @@
 package pl.edu.pw.mini.bms2;
 
+import au.com.bytecode.opencsv.CSVReader;
 import org.neuroph.core.Layer;
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.Neuron;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.comp.neuron.BiasNeuron;
 import org.neuroph.nnet.learning.MomentumBackpropagation;
 import org.neuroph.util.NeuronProperties;
 import org.neuroph.util.TransferFunctionType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
@@ -85,8 +92,50 @@ public class Main {
         momentumBackpropagation.setMomentum(momentumRatio);
         momentumBackpropagation.setMaxIterations(maxIterations);
 
+        //problem type setting
+        System.out.println("Choose problem type:\n" +
+                        "0:\tRegression\n" +
+                        "1:\tClustering"
+        );
+        int problemType = in.nextInt();
+
+        System.out.println("Type location of training set file:\n");
+        String trainingSetFilePath = in.next();
+        DataSet traingSet = CreateTrainingSet(trainingSetFilePath, problemType);
+
         //creating network
         NeuralNetwork nn = new MultiLayerPerceptron(neuronsInLayers, np);
         nn.setLearningRule(momentumBackpropagation);
+    }
+
+    private static DataSet CreateTrainingSet(String trainingSetFilePath, int problemType) {
+        DataSet trainingSet =  new DataSet(problemType + 1, 1);
+
+        CSVReader reader = null;
+        try {
+            reader = new CSVReader(new FileReader(trainingSetFilePath));
+            String [] nextLine;
+
+            //read headers
+            System.out.println(reader.readNext().toString());
+            while ((nextLine = reader.readNext()) != null) {
+                int length = nextLine.length;
+                double[] input = new double[length-1];
+
+                for (int i = 0; i < length-1; i++) {
+                    input[i] = Double.parseDouble(nextLine[i]);
+                }
+
+                double[] output = new double[]{Double.parseDouble(nextLine[length - 1])};
+
+                trainingSet.addRow(input, output);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return trainingSet;
     }
 }
